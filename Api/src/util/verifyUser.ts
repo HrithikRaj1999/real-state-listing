cb49a22cdee9c5a4d1b3e4ab3c87a89258b995de
Hrithik Raj <hrkraj99@gmail.com>
2024-03-31 12:17:13 +0530

    minor:loggers and env added

--- Api/src/util/verifyUser.ts
+++ Api/src/util/verifyUser.ts
@@ -2,19 +2,20 @@ import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { HTTP_STATUS_CODES, HTTP_STATUS_MESSAGE } from "../constants/data";
-
+import { logger } from "../logger/logger";
 
 export const verifyToken = (
   req: Request,
   res: Response,
-  next: NextFunction,
+  next: NextFunction
 ) => {
   const token = req.cookies.access_token;
 
   if (!token) {
+    logger.error("User is invalid");
     return next(createHttpError(401, "Unauthorized"));
   }
-
+  logger.info("user is valid", token);
   jwt.verify(
     token,
     process.env.JWT_SECRET!,
@@ -23,22 +24,22 @@ export const verifyToken = (
         return next(
           createHttpError(
             HTTP_STATUS_CODES.FORBIDDEN,
-            HTTP_STATUS_MESSAGE.FORBIDDEN,
-          ),
+            HTTP_STATUS_MESSAGE.FORBIDDEN
+          )
         );
       }
       if (!payload || typeof payload !== "object") {
         return next(
           createHttpError(
             HTTP_STATUS_CODES.FORBIDDEN,
-            HTTP_STATUS_MESSAGE.FORBIDDEN,
-          ),
+            HTTP_STATUS_MESSAGE.FORBIDDEN
+          )
         );
       }
-      const user = payload as JwtPayload; 
+      const user = payload as JwtPayload;
 
-      req.body.tokenUserId = user.id; 
+      req.body.tokenUserId = user.id;
       next();
-    },
+    }
   );
 };
